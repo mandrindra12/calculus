3753ead13dd56a89efb7ebe5d95c89c79bbc67c4
mandrindra12 <mandrindraantonnio@gmail.com>
2023-10-31 16:10:03 +0300

    main to run some test

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
-#include "main_utils.h"
+#include "resolution.h"
+#include "function.h"
+#include "calculus.h"
 #include <stdio.h>
 #include <math.h>
-#define EPSILON 1e-6 
+#define EPS 1e-12 
 
 int main(int argc, char **argv) {
 
         double a = 0., res = 0.0, b = 0.0, n = 0.;
         printf("a, b, n: ");
         scanf("%lf%lf%lf", &a, &b, &n);
-        res = resolution(f, a, b, EPSILON);
-        printf("%lf\n",res);
-        res = rectangular_rule(f, a, b, n);
-        printf("RECT: %e\n", res);
-        res = trapezoidal_rule(f, a, b, n);
-        printf("TRAP: %lf %e %g\n", res, res, res);
+        res = dichotomie(f, a, b, EPS);
+        printf("dich: %lf %e %g\n", res, res, res);
+        res = secant_method(f, a, b, EPS);
+        printf("sec: %lf %e %g\n", res, res, res);
+        res  = newton_raphson(f, df, a, EPS);
+        printf("n-r: %lf %e %g\n", res, res, res);
         return 0;
 
 }
