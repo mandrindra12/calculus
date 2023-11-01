1ad72e4478e31f432e93b11193467e984c272d74
mandrindra12 <mandrindraantonnio@gmail.com>
2023-11-01 17:18:05 +0300

    test for some adjustment on calculus

--- main.c
+++ main.c
@@ -10,12 +10,17 @@ int main(int argc, char **argv) {
         double a = 0., res = 0.0, b = 0.0, n = 0.;
         printf("a, b, n: ");
         scanf("%lf%lf%lf", &a, &b, &n);
-        res = dichotomie(f, a, b, EPS);
-        printf("dich: %lf %e %g\n", res, res, res);
-        res = secant_method(f, a, b, EPS);
-        printf("sec: %lf %e %g\n", res, res, res);
-        res  = newton_raphson(f, df, a, EPS);
-        printf("n-r: %lf %e %g\n", res, res, res);
+        // res = dichotomie(f, a, b, EPS);
+        // printf("dich: %lf %e %g\n", res, res, res);
+        // res = secant_method(f, a, b, EPS);
+        // printf("sec: %lf %e %g\n", res, res, res);
+        // res  = newton_raphson(f, df, a, EPS);
+        // printf("n-r: %lf %e %g\n", res, res, res);
+        res = trapezoidal_rule(f, a, b, n);
+        printf("trap: %lf %e %g\n", res, res, res);
+        res = rectangular_rule(f, a, b, n);
+        printf("rect: %lf %e %g\n", res, res, res);
+        
         return 0;
 
 }
