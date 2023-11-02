a3e9431b589c9a54bb40a0c40601fb405f1094dc
mandrindra12 <mandrindraantonnio@gmail.com>
2023-11-02 06:05:33 +0300

    simpson's method added

--- calculus.c
+++ calculus.c
@@ -1,24 +1,40 @@
-#include "calculus.h"
 #include <math.h>
-double trapezoidal_rule(double (*fn)(double x), double i_start, double i_end, double n) {
+#include <stdint.h>
+#include <stdio.h>
+
+double trapezoidal_rule(double (*fn)(double x), double i_start, double i_end, uint32_t n) {
         
         double res = 0.;
         double seg = (i_end - i_start) / n;
         res = fn(i_start) + fn(i_end);
-        for(int i = 1; i < n-1; ++i){
+        for(int i = 1; i <= n-1; ++i){
                 res += 2*fn(i_start+i*seg);
         }
         res *= (seg/2);
-
         return fabs(res);
 }
 
-double rectangular_rule(double (*fn_ptr)(double x), double a, double b, double n) {
+double rectangular_rule(double (*fn_ptr)(double x), double a, double b, uint32_t n) {
        double res = fn_ptr(a);
        double i = (b-a)/n;
-       for(int j = 0; j < n-1; ++j) {
+       for(int j = 0; j <= n-1; ++j) {
                 res += fn_ptr(a+i*j);
        }
        return fabs(res)*i;
+}
+
+double simpson(double (*f)(double x), double i_start, double i_end, uint32_t n) {
+        double res = 0.;
+        res = f(i_end) + f(i_start);
+        double seg = (i_end - i_start) / n;
+        for(int i = 1; i <= n-1; i+=2) {
+                 res += 4*f(i_start+i*seg);
+        }
+        for( int i = 2; i <= n-2; i+=2) {
+                res+=2*f(i_start+i*seg);
+        }
+
+        res *= (seg/3);
+        return fabs(res);
 }
 
--- calculus.h
+++ calculus.h
@@ -1,7 +1,9 @@
 #ifndef _FONC_H_
 #define _FONC_H_
+#include <stdint.h>
 
-double rectangular_rule(double (*fn_ptr)(double x), double a, double b, double n);
-double trapezoidal_rule(double (*fn)(double x), double i_start, double i_end, double precision);
+double rectangular_rule(double (*fn_ptr)(double x), double a, double b, uint32_t n);
+double trapezoidal_rule(double (*fn)(double x), double i_start, double i_end, uint32_t n);
+double simpson(double (*f)(double x), double i_start, double i_end, uint32_t n);
 
 #endif
